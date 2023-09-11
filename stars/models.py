2a57a200090865bda6760833f549a6d5ce938ae4
Dmitriy Chekhonadskikh <Dmitriy.Che.git@protonmail.com>
2023-09-11 06:02:14 +0300

    django setup. django models added

---
+++ stars/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Stars(models,Model):
+    star_name = models.TextField(max_length=50)
+    star_constellation = models.CharField(max_length=50)
+    star_type = models.CharField(max_length=50)
+    star_distance = models.PositiveIntegerField(max_length=25)  #maybe use floats instead
+    star_mass = models.PositiveIntegerField(max_length=25)
+    star_temperature = models.TextField(max_length=25)
+    star_period = models.TextField(max_length=25)
+    star_turn_speed = models.TextField(max_length=25)
+
+    def __str__(self):
+        return f'Stars: {self.star_name} {self.star_constellation}'
+       
