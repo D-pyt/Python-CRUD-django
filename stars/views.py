59fceda8f82b0d37fae47402cb6c64f64d6cf7bd
Dmitriy Chekhonadskikh <Dmitriy.Che.git@protonmail.com>
2023-09-25 22:11:35 +0300

    requirments added, autovenv launch fix

--- stars/views.py
+++ stars/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Stars
 
 # Create your views here.
 def index(request):
-    return render(request, 'stars/index.html')
-
+    return render(request, 'stars/index.html',{
+        'stars': Stars.objects.all()
+    })
